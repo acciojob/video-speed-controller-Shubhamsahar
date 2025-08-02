0e0bea292668b0456936e5affbd4771a67634ec7
Shubhamsahar <kumarshubham765484@gmail.com>
2025-08-02 10:23:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-const inputs = document.querySelectorAll('.controls input');
+const player = document.querySelector('.player');
+const video = player.querySelector('.player__video');
+const playButton = player.querySelector('.toggle');
+const volumeSlider = player.querySelector('input[name="volume"]');
+const playbackSpeedSlider = player.querySelector('input[name="playbackRate"]');
+const skipBackwardButton = player.querySelector('.skip-backward');
+const skipForwardButton = player.querySelector('.skip-forward');
+const progressBar = player.querySelector('.progress__filled');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+video.addEventListener('timeupdate', updateProgressBar);
+playButton.addEventListener('click', togglePlay);
+volumeSlider.addEventListener('input', updateVolume);
+playbackSpeedSlider.addEventListener('input', updatePlaybackSpeed);
+skipBackwardButton.addEventListener('click', skipBackward);
+skipForwardButton.addEventListener('click', skipForward);
+
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+        playButton.textContent = '❚ ❚';
+    } else {
+        video.pause();
+        playButton.textContent = '►';
     }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateProgressBar() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.value = percent;
+}
+
+function updateVolume() {
+    video.volume = volumeSlider.value;
+}
+
+function updatePlaybackSpeed() {
+    video.playbackRate = playbackSpeedSlider.value;
+}
+
+function skipBackward() {
+    video.currentTime -= 10;
+}
+
+function skipForward() {
+    video.currentTime += 25;
+}
